879ac2873c93f21f280e9d7ea25853f057ea8f0e
Lauren Grieb <beverly220@ksu.edu>
2020-02-05 14:09:15 -0600

    Yee yee

--- DoesItBlend/Fruit.cs
+++ DoesItBlend/Fruit.cs
@@ -7,7 +7,13 @@ namespace DoesItBlend
     /// <summary>
     /// A base class representing fruit
     /// </summary>
-    public abstract class Fruit
+    public abstract class Fruit : IBlendable
     {
+        public virtual string Blend()
+        {
+            return "Goop";
+        }
+        //abstract means no body & use override; uses individual method ONLY
+        //virtual means body & use new or override in individual class; uses base class when necessary
     }
 }
--- DoesItBlend/Mango.cs
+++ DoesItBlend/Mango.cs
@@ -13,7 +13,7 @@ namespace DoesItBlend
         /// Blends the mango
         /// </summary>
         /// <returns>The result of blending a mango</returns>
-        public string Blend()
+        public new string Blend()
         {
             return "Orange Oddness";
         }
